c1a86a196640c5e09abda70434733ebc4d0dec4f
ngdevelop-tech <aprajapati06@gmail.com>
2018-01-03 23:40:33 +0530

    Model Driven Forms with Validation

--- src/app/add-product-model/add-product-model.component.ts
+++ src/app/add-product-model/add-product-model.component.ts
@@ -1,5 +1,7 @@
+import { ProductService } from './../service/product.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { Product } from '../models/product';
 @Component({
   selector: 'app-add-product-model',
   templateUrl: './add-product-model.component.html',
@@ -9,6 +11,7 @@ export class AddProductModelComponent implements OnInit {
 
   productTypes = ['Laptop', 'Mobile'];
 
+  formSubmitted = false;
   myForm: FormGroup;
 
   title: FormControl;
@@ -19,16 +22,16 @@ export class AddProductModelComponent implements OnInit {
   price: FormControl;
 
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
 
-    this.title = new FormControl();
+    this.title = new FormControl('', [Validators.required, Validators.minLength(10)]);
     this.modelName = new FormControl();
-    this.color = new FormControl();
-    this.productType = new FormControl();
-    this.brand = new FormControl();
-    this.price = new FormControl();
+    this.color = new FormControl('', Validators.pattern('[a-zA-Z]*'));
+    this.productType = new FormControl('', Validators.required);
+    this.brand = new FormControl('', Validators.required);
+    this.price = new FormControl('', [Validators.required, Validators.min(1)]);
 
     this.myForm = new FormGroup({
       'title': this.title,
@@ -38,6 +41,13 @@ export class AddProductModelComponent implements OnInit {
       'brand': this.brand,
       'price': this.price
     });
+
+  }
+
+  addProduct(product: Product) {
+
+    this.productService.addProduct(product);
+    this.formSubmitted = true;
 
   }
 
